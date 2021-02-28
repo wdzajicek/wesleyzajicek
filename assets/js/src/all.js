957c62d0a0375adf5054f34bd133151c2959356b
wdzajicek <tig.welder4@gmail.com>
2021-02-27 21:47:13 -0600

    Add mirrored bg. Add SVG for name.

--- assets/js/src/all.js
+++ assets/js/src/all.js
@@ -1,6 +1,8 @@
 import test from './test.js';
+import initTranslate from './translate.js';
 //import './babelTest.js';
 
 document.addEventListener('DOMContentLoaded', function() {  
   test();
+  initTranslate();
 });
